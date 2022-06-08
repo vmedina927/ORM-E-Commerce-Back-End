3715208115ad023cf82bebd4714e4a7d97913ecb
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-06-08 12:55:09 -0400

    Added code to Category.js file

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define a category_name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
