c5902008b524734d898d161c14644f3e470743c4
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-07-22 17:43:07 -0400

    Added code for ProductTag.js file

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,7 +7,34 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    // define product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the product model's id
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+
+    //define tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references the tag model's id
+      references: {
+        model: 'tag',
+        key: 'id'
+    }
+  }
   },
+  
   {
     sequelize,
     timestamps: false,
