2eb1975c0a032a3ba3f583c4964e7247314b81f7
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-07-23 14:23:40 -0400

    Added missing code to product-routes.js file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -59,16 +59,14 @@ router.get('/:id', (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+router.post('/', (req, res) => {  
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id:require.vody.category_id,
+    tagIds: req.body.tag_id
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
