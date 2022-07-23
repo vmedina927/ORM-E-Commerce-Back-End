c9158f67d99bc875febffeb4f18c079a7725ea82
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-07-23 14:25:38 -0400

    Fixed error in code

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -64,7 +64,7 @@ router.post('/', (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    category_id:require.vody.category_id,
+    category_id: req.body.category_id,
     tagIds: req.body.tag_id
   })
     .then((product) => {
