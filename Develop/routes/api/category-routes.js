2b1eba9dd03702bb19150f70210f86e7d2592690
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-07-23 15:59:15 -0400

    Fixed error when using the server

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbCategory => res.json(dbCategoryData))
+    .then(dbCategoryData => res.json(dbCategoryData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
