34b6a659e89be1c646bb0d73907578b9761b1de9
vmedina927 <98541583+vmedina927@users.noreply.github.com>
2022-07-23 16:03:40 -0400

    Fixed error in product-routes.js file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  ProductTag.findAll({
+  Product.findAll({
     include: [
       {
         model: Category,
